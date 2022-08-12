dc4516d21958d7326a49d5335a0dd45717d53728
Uday Kant <uday kant.udaykant187@gmail.com>
2022-08-12 23:32:26 +0530

    practice on this file

--- index.php
+++ index.php
@@ -18,5 +18,22 @@
     echo 'total(including space) = '.$size.'<br>';
     $url='<h4> url is ---> '.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'</h4>';
     echo $url;
+
+    $arr=['uday ','kant','thakur'];
+    echo sizeof($arr).'<br>';
+    echo $arr[0];
+    echo count($arr);
+    //$i=0;
+    foreach($arr as $arr1){
+        echo $arr1[0];        
+    }
+
+    echo '<br>reverse the string<br>';
+    $s="hello this is the string";
+    echo $s.'<br>';
+    $a=explode(' ',$s);
+    echo 'it has '.count($a).' words<br>';
+    //echo count_chars($s,1);
+    
 ?>
 </center>
